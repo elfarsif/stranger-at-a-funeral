0876158ffc41a924ac0bea290d4a31fa87ffa35d
Frank-Norris El Farsi <136501798+elfarsif@users.noreply.github.com>
2024-09-13 23:32:15 -0400

    feat: move main character upwards (#6)
    * feat : walking up
    
    * feat : walking up animation

--- core/src/main/java/io/github/elfarsif/Ressource.java
+++ core/src/main/java/io/github/elfarsif/Ressource.java
@@ -9,12 +9,21 @@ public class Ressource {
     Texture mainDownStanding3 = new Texture("mainCharacter/playerDownStanding3.png");
     Texture mainDownStanding4 = new Texture("mainCharacter/playerDownStanding4.png");
     Texture mainDownStanding5 = new Texture("mainCharacter/playerDownStanding5.png");
+    Texture mainDownStanding6 = new Texture("mainCharacter/playerDownStanding6.png");
 
     Texture mainWalkingDown1 = new Texture("mainCharacter/walkingDown/down1.png");
     Texture mainWalkingDown2 = new Texture("mainCharacter/walkingDown/down2.png");
     Texture mainWalkingDown3 = new Texture("mainCharacter/walkingDown/down3.png");
     Texture mainWalkingDown4 = new Texture("mainCharacter/walkingDown/down4.png");
     Texture mainWalkingDown5 = new Texture("mainCharacter/walkingDown/down5.png");
+    Texture mainWalkingDown6 = new Texture("mainCharacter/walkingDown/down6.png");
+
+    Texture mainWalkingUp1 = new Texture("mainCharacter/walkingUp/up1.png");
+    Texture mainWalkingUp2 = new Texture("mainCharacter/walkingUp/up2.png");
+    Texture mainWalkingUp3 = new Texture("mainCharacter/walkingUp/up3.png");
+    Texture mainWalkingUp4 = new Texture("mainCharacter/walkingUp/up4.png");
+    Texture mainWalkingUp5 = new Texture("mainCharacter/walkingUp/up5.png");
+    Texture mainWalkingUp6 = new Texture("mainCharacter/walkingUp/up6.png");
 
 
     public Texture getMainDownStanding1(){
@@ -37,6 +46,10 @@ public class Ressource {
         return mainDownStanding5;
     }
 
+    public Texture getMainDownStanding6(){
+        return mainDownStanding6;
+    }
+
     public Texture getMainWalkingDown1(){
         return mainWalkingDown1;
     }
@@ -52,4 +65,33 @@ public class Ressource {
     public Texture getMainWalkingDown5(){
         return mainWalkingDown5;
     }
+
+    public Texture getMainWalkingDown6(){
+        return mainWalkingDown6;
+    }
+
+    public Texture getMainWalkingUp1(){
+        return mainWalkingUp1;
+    }
+
+    public Texture getMainWalkingUp2(){
+        return mainWalkingUp2;
+    }
+
+    public Texture getMainWalkingUp3(){
+        return mainWalkingUp3;
+    }
+
+    public Texture getMainWalkingUp4(){
+        return mainWalkingUp4;
+    }
+
+    public Texture getMainWalkingUp5(){
+        return mainWalkingUp5;
+    }
+
+    public Texture getMainWalkingUp6(){
+        return mainWalkingUp6;
+    }
+
 }
