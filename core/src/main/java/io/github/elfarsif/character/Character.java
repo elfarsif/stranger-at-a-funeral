4beffb0cc9c3e1984558fa3ca8c89fa336fcb18e
elfarsif <f.n.elfarsi@gmail.com>
2024-09-15 19:24:45 -0400

    refactor : moved input Handling for character to seperate class

--- core/src/main/java/io/github/elfarsif/character/Character.java
+++ core/src/main/java/io/github/elfarsif/character/Character.java
@@ -5,38 +5,18 @@ import com.badlogic.gdx.Input;
 
 public class Character {
 
-    public TextureListInitializer textureListInitializer;
-    public TextureUpdater textureUpdater;
+    TextureListInitializer textureListInitializer;
+    TextureUpdater textureUpdater;
+    InputHandler inputHandler;
 
     public Character(){
         this.textureListInitializer = new TextureListInitializer();
         this.textureUpdater = new TextureUpdater(textureListInitializer);
+        this.inputHandler = new InputHandler(textureUpdater, textureListInitializer);
     }
 
     public void input(){
-        float speed = 1f;
-        float delta = Gdx.graphics.getDeltaTime();
-        boolean keyPressed = false;
-
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            this.textureUpdater.currentTextures= this.textureListInitializer.texturesWalkingUp;
-            this.textureUpdater.sprite.translateY(speed*delta);
-            keyPressed = true;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            this.textureUpdater.currentTextures = this.textureListInitializer.texturesWalking;
-            this.textureUpdater.sprite.translateY(-speed*delta);
-            keyPressed = true;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            this.textureUpdater.sprite.translateX(-speed*delta);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            this.textureUpdater.sprite.translateX(speed*delta);
-        }
-        if(!keyPressed){
-            this.textureUpdater.currentTextures = this.textureListInitializer.texturesStanding;
-        }
+        this.inputHandler.execute();
     }
 
 
---
+++ core/src/main/java/io/github/elfarsif/character/InputHandler.java
@@ -0,0 +1,51 @@
+package io.github.elfarsif.character;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
+public class InputHandler {
+    TextureUpdater textureUpdater;
+    TextureListInitializer textureListInitializer;
+    boolean keyPressed;
+
+    public InputHandler(TextureUpdater textureUpdater, TextureListInitializer textureListInitializer){
+        this.textureUpdater = textureUpdater;
+        this.textureListInitializer = textureListInitializer;
+    }
+
+    public void execute(){
+        float speed = 1f;
+        float delta = Gdx.graphics.getDeltaTime();
+        keyPressed = false;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            moveUp(speed, delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            moveDown(speed, delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            this.textureUpdater.sprite.translateX(-speed*delta);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            this.textureUpdater.sprite.translateX(speed*delta);
+        }
+        if(!keyPressed){
+            this.textureUpdater.currentTextures = this.textureListInitializer.texturesStanding;
+        }
+    }
+
+    private void moveUp(float speed, float delta) {
+        this.textureUpdater.currentTextures= this.textureListInitializer.texturesWalkingUp;
+        this.textureUpdater.sprite.translateY(speed * delta);
+        keyPressed = true;
+    }
+
+    private void moveDown(float speed, float delta) {
+        this.textureUpdater.currentTextures = this.textureListInitializer.texturesWalking;
+        this.textureUpdater.sprite.translateY(-speed * delta);
+        keyPressed = true;
+    }
+
+
+}
