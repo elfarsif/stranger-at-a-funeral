3978869900a351ded738135a969e086154d85e8b
elfarsif <f.n.elfarsi@gmail.com>
2025-02-08 12:08:35 -0500

    feat : reset to sample game

--- assets/tilemaps/main/Modern_Exteriors_Complete_Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Modern_Exteriors_Complete_Tileset" tilewidth="16" tileheight="16" tilecount="61952" columns="176">
- <image source="../../tilemapsAssets/Modern_Exteriors_Complete_Tileset.png" width="2816" height="5632"/>
-</tileset>
--- core/src/main/java/io/github/elfarsif/game/character/CHARACTER_STATE.java
+++
@@ -1,5 +0,0 @@
-package io.github.elfarsif.game.character;
-
-public enum CHARACTER_STATE {
-    STANDING
-}
